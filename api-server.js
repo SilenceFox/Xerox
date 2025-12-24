57fac3daa7b9cd652b9ee4871c3800e0127fbb3f
Jonathan Almeida <103967528+jl1A@users.noreply.github.com>
2025-12-24 11:14:58 -0300

    TestMain, Models and DB functions
    All the local files til now

---
+++ api-server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+// Importar controllers
+const professorController = require('../controllers/professor-controller');
+const departamentoController = require('../controllers/departamento-controller');
+// TO-DO: habilitar quando os controllers estiverem prontos
+// const impressaoController = require('../controllers/impressao-controller');
+
+// --------------------------------- Rotas ---------------------------------
+
+
+app.post('/api/professores/list', (req, res) => {
+    try {
+        // 
+        if (!req.body) {
+            return res.status(400).json({ error: 'Request body is required' });
+        }
+        const data = meuHandler(res);
+        res.status(201).json({professores: data});
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+module.exports = app;
